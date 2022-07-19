a4b339eda0b9a858bfa6e7af06a90976ef1f68eb
Rachael Bouissey <rbouissey@gmail.com>
2022-07-18 20:11:30 -0700

    trying to figure out how to trigger animation on keydown

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Globe } from './Globe';
 import { Kitty } from './Kitty';
 import './App.css';
 function App() {
--- src/Kitty.tsx
+++ src/Kitty.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import * as THREE from 'three';
+import { BackSide } from "three";
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
+
+
 
 
 			async function init(canvas: HTMLCanvasElement) {
@@ -11,12 +14,30 @@ import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
         const camera = new THREE.PerspectiveCamera(45, width/height, 1, 10000);
         camera.position.y = 200;
         camera.position.z = 400;
-        camera.lookAt(new THREE.Vector3(0,0,0));
         camera.aspect = canvas.clientWidth / canvas.clientHeight;
+        // camera.getWorldDirection(model.scene.position)
 
 				const scene = new THREE.Scene();
 
-        const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1.5 );
+        const lightH = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+        scene.add(lightH)
+        // scene.add( new THREE.AmbientLight( 0x222222 ) );
+
+				const light = new THREE.DirectionalLight( 0xffffff, 0.5 );
+				light.position.set( 200, 450, 500 );
+
+				light.castShadow = true;
+
+				light.shadow.mapSize.width = 1024;
+				light.shadow.mapSize.height = 512;
+
+				light.shadow.camera.near = 100;
+				light.shadow.camera.far = 1200;
+
+				light.shadow.camera.left = - 1000;
+				light.shadow.camera.right = 1000;
+				light.shadow.camera.top = 350;
+				light.shadow.camera.bottom = - 350;
         scene.add( light );
 
         const renderer = new THREE.WebGLRenderer( { antialias: true, canvas, alpha: true } );
@@ -30,27 +51,27 @@ import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
         // scene.add( sphere );
 
         const boxGeometry = new THREE.BoxGeometry( 2000, 2000, 2000 );
-        const boxMaterial = new THREE.MeshBasicMaterial( { color: "#ECF1F8" } );
+        const textureLoader = new THREE.TextureLoader();
+        const texture = textureLoader.load("sky.jpg")
+        const boxMaterial = new THREE.MeshBasicMaterial( { color: "#ECF1F8", side: BackSide, map: texture } );
         const cube = new THREE.Mesh( boxGeometry, boxMaterial );
+        
         cube.receiveShadow = true;
         cube.castShadow = true;
         scene.add( cube );
 
         const loader = new GLTFLoader();
-        let mixer: THREE.AnimationMixer
-        loader.load( 'cartoon-cats/glTF_pink_kawaii/CuteCat_glTF.gltf', gltf => {
-          mixer = new THREE.AnimationMixer( gltf.scene );
+        const model = await loader.loadAsync('cartoon-cats/glTF_pink_kawaii/CuteCat_glTF.gltf');
+        const mixer = new THREE.AnimationMixer( model.scene );
+        const run = model.animations.find(clip => clip.name === "run");
+        // const position = model.scene.getWorldPosition(model.scene.position);
 
-          //19 animations
-          gltf.animations.forEach(clip => {
-            console.log(clip.name)
-            clip.name === "ALLanim" && mixer.clipAction(clip).play()
-          })
-          gltf.scene.castShadow = true
-          scene.add(gltf.scene);
-          render()
-        });
+        model.scene.castShadow = true;
+        camera.lookAt(model.scene.position);
 
+        scene.add(model.scene);
+        render()
+        
 
         const planeGeometry = new THREE.PlaneGeometry( 2000, 2000, 32, 32 );
         const planeMaterial = new THREE.MeshToonMaterial( { color: "#238E47"} )
@@ -63,36 +84,77 @@ import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
         controls.enableDamping = true;
         controls.dampingFactor = 0.25;
         controls.enableZoom = true;
-        // controls.maxZoom = 100
-        // controls.maxDistance = 500
-        controls.maxPolarAngle = Math.PI/3;
+        controls.maxPolarAngle = Math.PI/3 * 1.5;
 
-        // const gridXZ = new THREE.GridHelper(100, 10, 0xff0000, 0xffffff);
-        // scene.add(gridXZ);
+
+
+
 
         renderer.setAnimationLoop(() => {
           renderer.render(scene, camera);
           controls.update();
-          if(mixer) mixer.update(0.04)
-      
+          if(mixer) mixer.update(0.04);
         })
 
         function render() {
           renderer.render( scene, camera );
         }
+
+        return {clip: run, mixer, model}
+
+
 			}
 
 
+
+
       export function Kitty() {
-        const [canvas, setCanvas] = React.useState<HTMLCanvasElement | null>(null)
-      
+        const [canvas, setCanvas] = React.useState<HTMLCanvasElement | null>(null);
+        const [clip, setClip] = React.useState<THREE.AnimationClip>();
+        const [model, setModel] = React.useState();
+        const [mixer, setMixer] = React.useState<THREE.AnimationMixer>();
+
+        const keyDownListener = () => (ev: KeyboardEvent) => {
+          ev.preventDefault()
+          console.log(clip)
+          console.log(mixer)
+
+          let distance = 1
+          while (ev.code === "KeyW") {
+            if(clip && mixer) mixer.clipAction(clip).play();
+            
+            // if(model) model.scene.translateX(distance ++);
+          }
+        }
+
+        const keyUpListener = (ev: KeyboardEvent) => {
+          ev.preventDefault()
+
+          if(ev.code === "KeyW"){
+            if(clip && mixer) mixer.clipAction(clip).stop();
+          }
+        }
+
+        const _init = async () => {
+          if(canvas){
+            const {clip, mixer, model} = await init(canvas);
+            setClip(clip);
+            setMixer(mixer);
+            // setModel(model);
+          }
+        }
+        
         React.useEffect(() => {
-         async function _init() {
-           if(canvas) {
-              await init(canvas)
-            }
-         }
-         _init()
+          document.addEventListener("keydown", keyDownListener);
+          document.addEventListener("keyup", keyUpListener);
+          _init();
+
+         return (
+          document.removeEventListener("keyup", keyUpListener),
+          document.removeEventListener("keydown", keyDownListener)
+         )
+
+
         },[canvas])
       
       return (
